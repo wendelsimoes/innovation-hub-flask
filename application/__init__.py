713a6a8f760b36b48824804ca60818efe9737430
wendelsimoes <wendel.simoes.br@gmail.com>
2022-06-02 17:54:49 -0300

    fix: removing circular statement

--- application/__init__.py
+++ application/__init__.py
@@ -13,5 +13,3 @@ login_manager.init_app(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///innovation-hub-flask.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
-
-import application.main
