d57fb207336793f1857f54b177a00fb5b8b1c82f
wendelsimoes <wendel.simoes.br@gmail.com>
2022-06-03 14:49:20 -0300

    fix: requirements and README

--- application/__init__.py
+++ application/__init__.py
@@ -2,18 +2,18 @@ from flask import Flask
 from flask_login import LoginManager, login_manager
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import mysql.connector
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "chave_secreta"
 
-# Configurar login geral para bloquar acesso de alguma páginas
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-# Configurar banco de dados
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///innovation-hub-flask.db"
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:021011wd@localhost/innovation-hub-flask"
+# Comente a linha abaixo se for usar o MySQL
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///innovation-hub-flask.db"
+# Comente a linha abaixo se for usar o Sqlite
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:021011wd@localhost/innovation-hub-flask"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
